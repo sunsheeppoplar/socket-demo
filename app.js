421f51b27b3ea64eee56401f76f7776e2087afdb
Abner Yang <abtyang@gmail.com>
2020-06-20 00:20:19 -0400

    Add Basic Server and Client Socket.io Connection
    - longpolling by default
    - same port as Express server
    - appends rudimentary socket info to DOM

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+const io = require('socket.io');
+
 const app = express();
 const router = express.Router();
-const path = require('path');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+const socketServer = io(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', router);
 
 router.get('/', (req, res) => {
     res.sendFile('index');
 })
 
-app.use('/', router);
-app.listen(port, () => console.log(`listening on port ${port}`));
+socketServer.on('connection', (socket) => {
+  const logMessages = `
+  id: ${socket.id}
+  pid: ${process.pid}`
+
+  console.log(`=== server socket connected === ${logMessages}`)
+})
+
+server.listen(port, () => console.log(`listening on port ${port}`));
--- public/index.js
+++ public/index.js
@@ -1,1 +1,35 @@
-console.log('hi')
+document.addEventListener('DOMContentLoaded', (e) => {
+  const socketClient = io.connect(`${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.host}`);
+  
+  const body = document.querySelector('body');
+  console.log(body, 'body')
+  
+  const appendInfo = (message) => {
+    const div = document.createElement('div')
+    div.innerText = message;
+    console.log(body, 'body')
+    body.appendChild(div);
+  }
+  
+  socketClient.on('connect', () => {
+    const socketInfo = `
+    --- client socket connected ---
+    id: ${socketClient.id}
+    time: ${new Date()}
+    -------------------`;
+  
+    appendInfo(socketInfo);
+  });
+  
+  socketClient.on('disconnect', (reason) => {
+    const socketInfo = `
+    --- client socket disconnected ---
+    id: ${socketClient.id}
+    reason: ${reason}
+    time: ${new Date()}
+    -------------------`;
+  
+    appendInfo(socketInfo);
+  });
+});
+
